f46e8de7a1fdb57288b815309e5b5a7d4856e134
Leena Abd Alrahman <leenasba02@gmail.com>
2026-09-11 19:53:26 +0300

    feat: add product seed data and image assets

---
+++ backend/database-schema.sql
@@ -0,0 +1,287 @@
+﻿CREATE TABLE [Roles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Users] (
+    [Id] nvarchar(450) NOT NULL,
+    [FullName] nvarchar(max) NULL,
+    [Address] nvarchar(max) NULL,
+    [City] nvarchar(max) NULL,
+    [CodeResetPassword] nvarchar(max) NULL,
+    [ExpireResetPassword] datetime2 NULL,
+    [RefreshToken] nvarchar(max) NULL,
+    [RefreshTokenExpiryTime] datetime2 NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [RoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_RoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_RoleClaims_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [Categories] (
+    [Id] int NOT NULL IDENTITY,
+    [Status] int NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    [CreatedBy] nvarchar(450) NULL,
+    [UpdatedBy] nvarchar(max) NULL,
+    [UpdatedAt] datetime2 NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Categories_Users_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [Users] ([Id])
+);
+GO
+
+
+CREATE TABLE [Orders] (
+    [Id] int NOT NULL IDENTITY,
+    [OrderStatus] int NOT NULL,
+    [UserId] nvarchar(450) NULL,
+    [paymentMethod] int NOT NULL,
+    [PaymentStatus] int NOT NULL,
+    [SessionId] nvarchar(max) NULL,
+    [PaymentId] nvarchar(max) NULL,
+    [AmountPaid] decimal(18,2) NOT NULL,
+    [OrderDate] datetime2 NOT NULL,
+    [ShippedDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Orders_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id])
+);
+GO
+
+
+CREATE TABLE [UserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_UserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_UserClaims_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [UserLogins] (
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [ProviderKey] nvarchar(450) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_UserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_UserLogins_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [UserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_UserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [UserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [Name] nvarchar(450) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_UserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_UserTokens_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [CategoryTranslations] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Language] nvarchar(max) NOT NULL,
+    [CategoryId] int NOT NULL,
+    CONSTRAINT [PK_CategoryTranslations] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_CategoryTranslations_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [Products] (
+    [Id] int NOT NULL IDENTITY,
+    [Price] decimal(18,2) NOT NULL,
+    [Stock] int NOT NULL,
+    [Rate] int NOT NULL,
+    [MainImage] nvarchar(max) NOT NULL,
+    [Quantity] int NOT NULL,
+    [CategoryId] int NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    [Discount] int NOT NULL,
+    [Status] int NOT NULL,
+    [CreatedBy] nvarchar(450) NULL,
+    [UpdatedBy] nvarchar(max) NULL,
+    [UpdatedAt] datetime2 NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Products_Users_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [Users] ([Id])
+);
+GO
+
+
+CREATE TABLE [Carts] (
+    [ProductId] int NOT NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    [Count] int NOT NULL,
+    CONSTRAINT [PK_Carts] PRIMARY KEY ([ProductId], [UserId]),
+    CONSTRAINT [FK_Carts_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Carts_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id])
+);
+GO
+
+
+CREATE TABLE [OrderItems] (
+    [ProductId] int NOT NULL,
+    [OrderId] int NOT NULL,
+    [Quantity] int NOT NULL,
+    [UnitPrice] decimal(18,2) NOT NULL,
+    [TotalPrice] decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_OrderItems] PRIMARY KEY ([OrderId], [ProductId]),
+    CONSTRAINT [FK_OrderItems_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([Id]),
+    CONSTRAINT [FK_OrderItems_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [ProductImages] (
+    [Id] int NOT NULL IDENTITY,
+    [ProductId] int NOT NULL,
+    [ImageName] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_ProductImages] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProductImages_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [ProductTranslations] (
+    [Id] int NOT NULL IDENTITY,
+    [ProductId] int NOT NULL,
+    [Language] nvarchar(max) NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_ProductTranslations] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProductTranslations_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [Reviews] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ProductId] int NOT NULL,
+    [Rating] int NOT NULL,
+    [Comment] nvarchar(max) NOT NULL,
+    [DateTime] datetime2 NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    CONSTRAINT [PK_Reviews] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Reviews_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Reviews_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE INDEX [IX_Carts_UserId] ON [Carts] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_Categories_CreatedBy] ON [Categories] ([CreatedBy]);
+GO
+
+
+CREATE INDEX [IX_CategoryTranslations_CategoryId] ON [CategoryTranslations] ([CategoryId]);
+GO
+
+
+CREATE INDEX [IX_OrderItems_ProductId] ON [OrderItems] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Orders_UserId] ON [Orders] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_ProductImages_ProductId] ON [ProductImages] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Products_CategoryId] ON [Products] ([CategoryId]);
+GO
+
+
+CREATE INDEX [IX_Products_CreatedBy] ON [Products] ([CreatedBy]);
+GO
+
+
+CREATE INDEX [IX_ProductTranslations_ProductId] ON [ProductTranslations] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Reviews_ProductId] ON [Reviews] ([ProductId]);
+GO
+
+
+CREATE INDEX [IX_Reviews_UserId] ON [Reviews] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_RoleClaims_RoleId] ON [RoleClaims] ([RoleId]);
+GO
+
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [Roles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+GO
+
+
+CREATE INDEX [IX_UserClaims_UserId] ON [UserClaims] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_UserLogins_UserId] ON [UserLogins] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_UserRoles_RoleId] ON [UserRoles] ([RoleId]);
+GO
+
+
+CREATE INDEX [EmailIndex] ON [Users] ([NormalizedEmail]);
+GO
+
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [Users] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+GO
+
+
